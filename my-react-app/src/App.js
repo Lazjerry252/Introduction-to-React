6426503c10f42d11b23366aa7c80d37e09e67c49
jerry252 <lazarus.com>
2024-07-25 11:52:19 -0700

    initial commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import './App.css';
+import AppHeader from "./AppHeader";
+import AppMainContent from "./AppMainContent";
+
+
+
+const App = () => {
+
+  return (
+    <div className="container">
+      <AppHeader />
+      <AppMainContent />
+    </div>
+  )
+
+}
+
+export default App
